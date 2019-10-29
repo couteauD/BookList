6573ec5a6bc93fcf546313262f570b0070ea97ba
couteauD <598731667@qq.com>
2019-10-29 18:37:05 +0800

    “新闻”Tab上集成WebView显示网页news.sina.cn

--- app/src/main/java/com/casper/testdrivendevelopment/data/NewsFragment.java
+++ app/src/main/java/com/casper/testdrivendevelopment/data/NewsFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.casper.testdrivendevelopment.R;
 
@@ -25,6 +27,14 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_news, container, false);
+        WebView webView=(WebView)view.findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        //打开DOM储存API
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("http://news.sina.cn/");
+
+
         return view;
     }
 
